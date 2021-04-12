66137d789abf48d61374364136a65026469ad37f
Piotr Gustab <pgustab@aol.com>
2021-04-11 21:19:17 -0400

    script.js playerMove updated-2

---
+++ js/script.js
@@ -0,0 +1,16 @@
+let computerMove = `kamień`;
+
+printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to papier, to wygrywasz!');
+
+let playerMove = 'papier';
+
+printMessage('Zagrałem' + playerMove + '! Jeśli Twój ruch to kamień, to wygrywasz!')
+
+
+
+//W ostatnim przykładzie zapisaliśmy słowo 'kamień' w zmiennej computerMove. Następnie, wykorzystaliśmy tę zmienną w tekście wyświetlanym na stronie.
+
+//Twoim zadaniem jest wykonanie takiej samej zmiany dla słowa 'papier', które zapiszesz w innej zmiennej – playerMove.
+
+//Po wykonaniu zadania umieść kod JS w pliku js/script.js, zapisz commit i wykonaj push. Następnie na stronie GitHuba znajdź w swoim projekcie najnowszy commit i wyślij mentorowi link do tego commita.
+
